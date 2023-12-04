8aa33ceb6fe01956bcc7eac7339b8216cdc8970d
holzmanj <jesseholzman96@gmail.com>
2023-12-04 14:10:06 -0500

    Implement day 1 part 2

--- src/solvers/day01.rs
+++ src/solvers/day01.rs
@@ -19,6 +19,22 @@ pub fn part1(input: &String) -> String {
         .to_string()
 }
 
-pub fn part2(_input: &String) -> String {
-    "Not implemented".to_string()
+pub fn part2(input: &String) -> String {
+    let words = [
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+
+    let mut modified_input = String::new();
+
+    for (i, char) in input.chars().enumerate() {
+        for (word_i, word) in words.iter().enumerate() {
+            if input.get(i..).unwrap().starts_with(word) {
+                let digit = format!("{}", word_i + 1);
+                modified_input.push_str(digit.as_str());
+            }
+        }
+        modified_input.push(char);
+    }
+
+    part1(&modified_input)
 }
