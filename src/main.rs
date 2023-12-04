72b967c6a6cffebe6d59892232c6c5c5fb3931f4
Jesse Holzman <jesseholzman96@gmail.com>
2023-12-04 07:55:29 -0500

    Initial solution for day 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::{fs, println};
+
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string("inputs/01.txt").expect("Failed to read input file");
+
+    let line_nums = input.lines().map(|l| {
+        l.chars()
+            .filter(|c| c.is_digit(10) || c.is_whitespace())
+            .map(|c| c.to_digit(10).unwrap())
+    });
+
+    let ans: u32 = line_nums.clone()
+        .map(|mut ln| ln.nth(0).unwrap())
+        .zip(line_nums.map(|ln| ln.last().unwrap()))
+        .map(|(x, y)| x * 10 + y)
+        .sum();
+
+    println!("{ans}");
 }
