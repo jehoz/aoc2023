70df3a646eb45c7ab218511dc3f604aa0a6f3407
holzmanj <jesseholzman96@gmail.com>
2023-12-06 15:02:26 -0500

    Implement day 3 part 1

--- src/solvers/day03.rs
+++ src/solvers/day03.rs
@@ -1,8 +1,91 @@
+use std::collections::HashMap;
+
+use nom::{character::complete::digit1, error::Error};
+
 use crate::solvers::Solution;
 
-pub fn solve(_input: &String) -> Solution {
+pub fn solve(input: &String) -> Solution {
+    let parts = parse_parts(input);
+
+    let p1_ans = {
+        let mut nums: Vec<PartNumber> = parts
+            .iter()
+            .flat_map(|p| p.numbers.iter())
+            .cloned()
+            .collect();
+        nums.sort();
+        nums.dedup();
+        nums.iter().map(|n| n.value).sum::<u32>().to_string()
+    };
+
     Solution {
-        part1: "Not implemented".to_string(),
         part2: "Not implemented".to_string(),
+        part1: p1_ans,
     }
+}
+
+#[derive(Clone, PartialEq, Eq, Ord, PartialOrd)]
+struct PartNumber {
+    uid: u32,
+    value: u32,
+}
+
+struct Part {
+    symbol: char,
+    numbers: Vec<PartNumber>,
+}
+
+fn parse_parts(input: &str) -> Vec<Part> {
+    type Coord = (i32, i32);
+
+    let mut uid: u32 = 0;
+    let mut num_locs = HashMap::<Coord, PartNumber>::new();
+    let mut part_locs = Vec::<(char, Coord)>::new();
+
+    for (y, line) in (0i32..).zip(input.lines()) {
+        let mut x = 0;
+        while x < line.len() {
+            let ch = line.chars().nth(x).unwrap();
+
+            if ch.is_digit(10) {
+                let (_, num_str) =
+                    digit1::<&str, Error<&str>>(line.get((x as usize)..).unwrap()).unwrap();
+                let value = num_str.parse::<u32>().unwrap();
+                // map number to coordinate of each of its digits
+                for _ in num_str.chars() {
+                    num_locs.insert(((x as i32), y), PartNumber { uid, value });
+                    x += 1;
+                }
+                uid += 1;
+                continue;
+            } else if ch != '.' {
+                part_locs.push((ch, ((x as i32), y)));
+            }
+
+            x += 1;
+        }
+    }
+
+    // get any numbers in cells adjacent to part coordinate
+    let mut parts = Vec::new();
+    for (symbol, (x, y)) in part_locs {
+        let mut part = Part {
+            symbol,
+            numbers: Vec::new(),
+        };
+        for i in -1..=1 {
+            for j in -1..=1 {
+                if i == 0 && j == 0 {
+                    continue;
+                }
+
+                if let Some(n) = num_locs.get(&(x + i, y + j)) {
+                    part.numbers.push(n.clone());
+                }
+            }
+        }
+        parts.push(part);
+    }
+
+    parts
 }
