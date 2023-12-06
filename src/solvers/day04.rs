126eec672b60002b42506758ea63dbf19ded30d3
holzmanj <jesseholzman96@gmail.com>
2023-12-06 16:10:08 -0500

    Simplify day 4 part 1

--- src/solvers/day04.rs
+++ src/solvers/day04.rs
@@ -1,32 +1,20 @@
-use nom::{
-    bytes::complete::tag,
-    character::complete::{digit1, space1},
-    sequence::{delimited, tuple},
-    IResult,
-};
-
 use crate::solvers::Solution;
 
 pub fn solve(input: &str) -> Solution {
-    let cards: Vec<Card> = input
+    let num_matching: Vec<u32> = input
         .lines()
-        .map(|line| parse_card(line).unwrap().1)
-        .collect();
-
-    let part1 = cards
-        .iter()
+        .map(parse_card)
         .map(|card| {
-            let matching = card
-                .given_numbers
+            card.given_numbers
                 .iter()
                 .filter(|n| card.winning_numbers.contains(n))
-                .count();
-            if matching == 0 {
-                0
-            } else {
-                2u32.pow(matching as u32 - 1)
-            }
+                .count() as u32
         })
+        .collect();
+
+    let part1 = num_matching
+        .iter()
+        .map(|&n| if n == 0 { 0 } else { 2u32.pow(n as u32 - 1) })
         .sum::<u32>()
         .to_string();
 
@@ -37,24 +25,19 @@ pub fn solve(input: &str) -> Solution {
 }
 
 struct Card {
-    id: u32,
     winning_numbers: Vec<u32>,
     given_numbers: Vec<u32>,
 }
 
-fn parse_card(input: &str) -> IResult<&str, Card> {
-    let (input, card_id) = delimited(tuple((tag("Card"), space1)), digit1, tag(": "))(input)?;
+fn parse_card(input: &str) -> Card {
+    let input = input.split_once(":").unwrap().1;
 
     let mut numbers = input
         .split("|")
         .map(|nums| nums.split_whitespace().map(|n| n.parse::<u32>().unwrap()));
 
-    Ok((
-        input,
-        Card {
-            id: card_id.parse::<u32>().unwrap(),
-            winning_numbers: numbers.next().unwrap().collect(),
-            given_numbers: numbers.next().unwrap().collect(),
-        },
-    ))
+    Card {
+        winning_numbers: numbers.next().unwrap().collect(),
+        given_numbers: numbers.next().unwrap().collect(),
+    }
 }
