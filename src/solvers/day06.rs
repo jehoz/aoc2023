e515370d4a679a475a09ed6d00088a277625c13b
holzmanj <jesseholzman96@gmail.com>
2023-12-09 20:34:34 -0500

    Implement day 6 part 2

--- src/solvers/day06.rs
+++ src/solvers/day06.rs
@@ -10,51 +10,66 @@ use nom::{
 use crate::solvers::Solution;
 
 pub fn solve(input: &str) -> Solution {
-    let (_, records) = parse_records(input).unwrap();
-
-    let part1 = records
+    let part1 = parse_records(input)
         .iter()
-        .map(|r| {
-            let (s1, s2) = quadratic_roots(-1.0, r.time as f32, -(r.distance as f32));
-            f32::ceil(s2) - f32::floor(s1) - 1.0
-        })
-        .fold(1f32, |a, b| a * b)
+        .map(ways_to_beat)
+        .fold(1, |a, b| a * b)
         .to_string();
-    let part2 = "Not implemented".to_string();
+
+    let part2 = ways_to_beat(&parse_single_record(input)).to_string();
 
     Solution { part1, part2 }
 }
 
-fn quadratic_roots(a: f32, b: f32, c: f32) -> (f32, f32) {
-    let det = f32::sqrt(b.powi(2) - 4.0 * a * c);
+fn quadratic_roots(a: f64, b: f64, c: f64) -> (f64, f64) {
+    let det = f64::sqrt(b.powi(2) - 4.0 * a * c);
     (((-b) + det) / (2.0 * a), ((-b) - det) / (2.0 * a))
 }
 
-struct RaceRecord {
-    time: u32,
-    distance: u32,
+fn ways_to_beat(r: &RaceRecord) -> u64 {
+    let (s1, s2) = quadratic_roots(-1.0, r.time as f64, -(r.distance as f64));
+    (f64::ceil(s2) - f64::floor(s1) - 1.0) as u64
 }
 
-fn parse_records(input: &str) -> IResult<&str, Vec<RaceRecord>> {
-    let mut lines = input.lines();
-    let (_, times) = preceded(
-        pair(tag("Time:"), space1),
-        separated_list1(space1, map_res(digit1, |s: &str| s.parse::<u32>())),
-    )(lines.next().unwrap())?;
-    let (_, distances) = preceded(
-        pair(tag("Distance:"), space1),
-        separated_list1(space1, map_res(digit1, |s: &str| s.parse::<u32>())),
-    )(lines.next().unwrap())?;
+struct RaceRecord {
+    time: u64,
+    distance: u64,
+}
 
-    Ok((
-        input,
-        times
-            .into_iter()
-            .zip(distances)
-            .map(|(t, d)| RaceRecord {
-                time: t,
-                distance: d,
-            })
-            .collect(),
-    ))
+fn parse_nums<'a>(prefix: &str, input: &'a str) -> IResult<&'a str, Vec<u64>> {
+    preceded(
+        pair(tag(prefix), space1),
+        separated_list1(space1, map_res(digit1, |s: &str| s.parse::<u64>())),
+    )(input)
+}
+
+fn parse_records(input: &str) -> Vec<RaceRecord> {
+    let mut lines = input.lines();
+    let (_, times) = parse_nums("Time:", lines.next().unwrap()).unwrap();
+    let (_, distances) = parse_nums("Distance:", lines.next().unwrap()).unwrap();
+
+    times
+        .into_iter()
+        .zip(distances)
+        .map(|(t, d)| RaceRecord {
+            time: t,
+            distance: d,
+        })
+        .collect()
+}
+
+fn parse_single_record(input: &str) -> RaceRecord {
+    let parse = |line: &str| {
+        line.chars()
+            .filter(char::is_ascii_digit)
+            .collect::<String>()
+            .parse::<u64>()
+            .unwrap()
+    };
+
+    let mut lines = input.lines();
+    let time = parse(lines.next().unwrap());
+    let distance = parse(lines.next().unwrap());
+
+    RaceRecord { time, distance }
 }
